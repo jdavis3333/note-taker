72d552bebcda11f8bd1bbffeb63827f35ec54fa7
jdavis3333 <jdavis3333@gmail.com>
2020-07-16 19:47:08 -0700

    completed saveNote function

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,5 +1,5 @@
 var router = require("express").Router();
-var db = require("../db/db.json");
+var db = [];
 var path = require("path");
 
 router.get("/", function(req, res){
@@ -8,4 +8,27 @@ router.get("/", function(req, res){
 
 router.get("/notes", function(req, res){
     res.sendFile(path.join(__dirname, "../public/notes.html"))
+})
+
+router.get("/api/notes", function(req, res){
+    db = JSON.parse(fs.readFileSync("./db/db.json"));
+    console.log(db);
+    res.json(db);
+})
+
+router.post("/api/notes", function(req, res){
+    var record = {
+        id: db.length + Math.floor(Math.random()*100), 
+        title: req.body.title,
+        text: req.body.text,
+    }
+    db.push(record);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(db), function(err){
+        if(err) {
+            throw err
+        }
+        console.log(db)
+        res.json(db)
+    });
 })
