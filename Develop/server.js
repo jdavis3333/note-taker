0fd7876b222d08fcd82c5926846e2be26591ad48
jdavis3333 <jdavis3333@gmail.com>
2020-07-16 19:27:39 -0700

    completed server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,13 @@
 var express = require("express");
 var app = express();
+var routes = require("./routes")
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+app.use(routes);
+app.listen(PORT, function(){
+    console.log("server listening on PORT:", PORT)
+})
